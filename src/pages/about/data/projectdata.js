7c09d41ca596266b57454146cb155d351a1d5ed2
kamalrossi <rossikamal@gmail.com>
2022-07-03 21:10:38 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,47 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "KrazyLatino";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "KrazyLatino is a landing page for Responsive Restaurant Service. Major modules include menu, deal, reservation and homebanner"
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "CSS",
+    tag3: "HTML",
+    tag4: "FoodTech"
+  }
+];
+const projecttime = [
+  {
+    title: "UX & Prototyping",
+    from: "Jan 22",
+    to: "Feb 22"
+  },
+  {
+    title: "Responsive Software Development",
+    from: "Jan 22",
+    to: "Feb 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UX Design",
+    desc: "Prototyping Restaurant Website with Basic Functionalities"
+  },
+  {
+    title: "Responsive Web Development",
+    desc: "Design and Devlopment of Responsive Website Development"
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
